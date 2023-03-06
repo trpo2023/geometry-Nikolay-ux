6d6f0fc7ea6da25259153498e79dac4f8c8a88f9
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-03-06 13:18:59 +0700

    Add function check_args

--- main.c
+++ main.c
@@ -3,17 +3,66 @@
 
 #define N 50
 
-int check_name(char *string) {
+int check_name(char* string)
+{
     char temp[strlen(string)], len = 0;
-    for(int i = 0; i < strlen(string); i++) {
-        if(string[i] != '(') {
+    for (int i = 0; i < strlen(string); i++) {
+        if (string[i] != '(') {
             temp[i] = string[i];
             len = i;
-        }
-        else break;
+        } else
+            break;
     }
-    if(strcmp(temp, "circle") == 0) return len + 1;
-    else return 1;
+    if (strcmp(temp, "circle") == 0)
+        return len + 1;
+    else
+        return 1;
+}
+
+int check_args(char* string, char len)
+{
+    char arg_count = 0, arg2_count = 0, d = 0, b = 0;
+    for (char i = len; (i < strlen(string)) && (string[i] != ','); i++) {
+        if ((string[i] != ' ') && (string[i] != '.')
+            && (!((string[i] > 47) && (string[i] < 58)))) {
+            printf("Error at column %d", i);
+            return 1;
+        }
+        if ((string[i] > 47) && (string[i] < 58) && (string[i + 1] == ' '))
+            arg_count += 2;
+        else if ((string[i] == '.') && (string[i + 1] == ')')) {
+            arg_count += 3;
+            d = i;
+        }
+    }
+    if (arg_count > 2) {
+        printf("Error at column %d", d);
+        return 1;
+    }
+
+    for (char i = 0; i < strlen(string); i++) {
+        if ((string[i] == ',') && (d == 0)) {
+            d = i + 1;
+        }
+    }
+    for (char i = d; (string[i] != ')') && (i < strlen(string)); i++) {
+        if ((string[i] != ' ') && (string[i] != '.')
+            && (!((string[i] > 47) && (string[i] < 58)))) {
+            printf("Error at column %d", i);
+            return 1;
+        }
+        if ((string[i] > 47) && (string[i] < 58) && (string[i + 1] == ' '))
+            arg2_count += 1;
+        else if ((string[i] == '.') && (string[i + 1] == ')')) {
+            arg2_count += 2;
+            b = i;
+        }
+    }
+    if (arg2_count > 1) {
+        printf("Error at column %d", b);
+        return 1;
+    }
+    return 0;
 }
 
 int main()
@@ -22,11 +71,11 @@ int main()
     FILE* check;
     check = fopen("shapes.txt", "r");
     for (char count = 1; fgets(string, N, check) != NULL; count++) {
-        if(check_name(string) == 1) {
+        if (check_name(string) == 1) {
             printf("%s\n", string);
-            printf("error at column 1: expected 'circle'\n\n");
-        }
-        else printf("%s\n", string);
+            printf("error at column 0: expected 'circle'\n\n");
+        } else
+            printf("%s\n", string);
     }
     return 0;
 }
