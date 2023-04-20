0483b84662c4eee51579dc909bed87e924def299
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-04-21 03:52:23 +0700

    Add simple test

---
+++ test/test.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include "ctest.h"
+// #include "libgeometry/counter.h"
+
+#include "libgeometry/lexer.h"
+// #include "libgeometry/macr.h"
+// #include "libgeometry/parser.h"
+
+CTEST(check_figure_name, t1)
+{
+    char* str = "circle(1.0 1.0, 0)";
+    const int expected = strlen("circle");
+    const int real = check_name(str);
+    ASSERT_EQUAL(expected, real);
+}
