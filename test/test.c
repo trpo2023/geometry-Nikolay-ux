14a713a175f8b9e44414972b4ff9b81d15ab1bad
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-04-24 00:48:59 +0700

    Add test

--- test/test.c
+++ test/test.c
@@ -1,13 +1,11 @@
 #include <ctest.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctest.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 #include "libgeometry/counter.h"
 #include "libgeometry/lexer.h"
-#include "libgeometry/macr.h"
 #include "libgeometry/parser.h"
 
 CTEST(check_figure_name_ok, t1)
@@ -38,7 +36,7 @@ CTEST(check_args_ok, t3)
     int c = check_arg_y(str, &len);
     int d = check_radius(str, &len);
     int e = check_brace_2(str, &len);
-    if(a == 0 && b == 0 && c == 0 && d == 0 && e == 0) {
+    if (a == 0 && b == 0 && c == 0 && d == 0 && e == 0) {
         const int real = 0;
         ASSERT_EQUAL(expected, real);
     }
@@ -124,6 +122,20 @@ CTEST(check_second_brace_ok, t10)
 CTEST(check_second_brace_fail, t11)
 {
     char* str = "circle(1.0 1.0, 2(";
+    const int expected = 6;
+    int len = (int)strlen("circle");
+    check_brace_1(str, &len);
+    check_arg_x(str, &len);
+    check_arg_y(str, &len);
+    check_radius(str, &len);
+    check_brace_2(str, &len);
+    const int real = check_brace_2(str, &len);
+    ASSERT_EQUAL(expected, real);
+}
+
+CTEST(check_unexpected_fail, t11)
+{
+    char* str = "circle(1.0 1.0, 2) awd";
     const int expected = 7;
     int len = (int)strlen("circle");
     check_brace_1(str, &len);
@@ -135,21 +147,21 @@ CTEST(check_second_brace_fail, t11)
     ASSERT_EQUAL(expected, real);
 }
 
-CTEST(check_area_ok, t12)
+CTEST(check_area, t12)
 {
     char* str = "circle(1.0 1.0, 2)";
-    const double expected = M_PI*2*2;
+    const double expected = M_PI * 2 * 2;
     int len = (int)strlen("circle");
     check_brace_1(str, &len);
     check_arg_x(str, &len);
     check_arg_y(str, &len);
-    if(check_radius(str, &len) == 0) {
+    if (check_radius(str, &len) == 0) {
         const double real = count_area(str);
         ASSERT_DBL_NEAR_TOL(expected, real, 0.00000000001);
     }
 }
 
-CTEST(check_perimeter_ok, t12)
+CTEST(check_perimeter, t12)
 {
     char* str = "circle(1.0 1.0, 2)";
     const double expected = M_PI * 2 * 2;
